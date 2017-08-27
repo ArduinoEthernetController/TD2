0d3a13cc80a9dcbffb5d7e0d7b9eb4695a01c1a7
Alexander Harl <a.harl@symanics.com>
2017-08-27 20:15:50 +0200

    first commit

---
+++ Assets/WorldManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+
+    public GameObject GroundNode;
+    public GameObject PathNode;
+
+    private string levelString = "[1,1,1,1,1,1,1,2]";
+
+    // Use this for initialization
+    void Start () {
+        int[] l = JsonUtility.FromJson<int[]>(levelString);
+        foreach(int i in l)
+        {
+            Debug.LogError(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour {
+    [Header("Attributes")]
+    public Color hovercolor;
+    public Vector3 offset;
+
+    [Header("Optional")]
+    public GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+    BuildManager buildManager;
+
+    public Vector3 GetBuildPosition()
+        {
+        return transform.position + offset;
+        }
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        buildManager = BuildManager.instance;
+	}
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+             return;
+        
+
+        if (!buildManager.CanBuild)
+            return;
+
+        if(turret != null)
+        {
+            Debug.Log("Can't build there! ToDo : Display");
+            return;
+        }
+        buildManager.BuildTurretOn(this);
+
+        //build a turret
+    }
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+
+        if (!buildManager.CanBuild)
+            return;
+
+        rend.material.color = hovercolor;
+
+    }
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+ 
+
+
+}
